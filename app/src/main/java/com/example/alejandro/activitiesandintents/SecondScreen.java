a0a8d79b37af2c53562bd73916740400e7583809
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-04-02 17:57:38 -0500

    added changes for receiving simple text data on second activity

--- app/src/main/java/com/example/alejandro/activitiesandintents/SecondScreen.java
+++ app/src/main/java/com/example/alejandro/activitiesandintents/SecondScreen.java
@@ -6,19 +6,34 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by alejandro on 4/2/18.
  */
 
 public class SecondScreen extends AppCompatActivity {
+    TextView receivedText;
+    Button homeScreen;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_screen);
 
-        Button fstScreenBtn = (Button) findViewById(R.id.goToFstScreenBtn);
-        fstScreenBtn.setOnClickListener(new View.OnClickListener() {
+        Intent callingIntent = getIntent();
+        String intentAction = callingIntent.getAction();
+        String intentType = callingIntent.getType();
+
+        if (Intent.ACTION_SEND.equals(intentAction) && intentType != null){
+            if (intentType.equals("text/plain")){
+                handleReceivedText(callingIntent);
+            }
+        }
+
+        homeScreen = (Button) findViewById(R.id.goToFstScreenBtn);
+        homeScreen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent fstScreenIntent = new Intent(getApplicationContext(), MainActivity.class);
@@ -26,5 +41,14 @@ public class SecondScreen extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void handleReceivedText(Intent intent){
+        receivedText = (TextView)findViewById(R.id.textReceived);
+        String intentText = intent.getStringExtra(Intent.EXTRA_TEXT);
+
+        if (receivedText != null){
+            receivedText.setText(intentText);
+        }
     }
 }
