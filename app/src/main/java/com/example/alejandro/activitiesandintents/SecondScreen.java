0e06a50ce4419fca6eb406c54ac4ad85b1953c18
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-04-02 02:20:36 -0500

    added onclick listener for navigation button snd screen

--- app/src/main/java/com/example/alejandro/activitiesandintents/SecondScreen.java
+++ app/src/main/java/com/example/alejandro/activitiesandintents/SecondScreen.java
@@ -1,8 +1,11 @@
 package com.example.alejandro.activitiesandintents;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by alejandro on 4/2/18.
@@ -13,5 +16,15 @@ public class SecondScreen extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_screen);
+
+        Button fstScreenBtn = (Button) findViewById(R.id.goToFstScreenBtn);
+        fstScreenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent fstScreenIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(fstScreenIntent);
+            }
+        });
+
     }
 }
