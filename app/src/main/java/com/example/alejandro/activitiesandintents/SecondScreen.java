5a3359b4befd1859acf662b88ffa4458e636f0cc
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-04-02 01:34:50 -0500

    added second activity and layout files

---
+++ app/src/main/java/com/example/alejandro/activitiesandintents/SecondScreen.java
@@ -0,0 +1,17 @@
+package com.example.alejandro.activitiesandintents;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by alejandro on 4/2/18.
+ */
+
+public class SecondScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
