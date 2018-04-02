59cbb08181c559a7d26473707a113b8289d5e950
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-04-02 17:50:46 -0500

    added changes for sending simple data example

--- app/src/main/java/com/example/alejandro/activitiesandintents/MainActivity.java
+++ app/src/main/java/com/example/alejandro/activitiesandintents/MainActivity.java
@@ -5,21 +5,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText sendText;
+    Button sendBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sendBtn = (Button)findViewById(R.id.goToSndScreenBtn);
 
-        Button sndScreen = (Button)findViewById(R.id.goToSndScreenBtn);
-        sndScreen.setOnClickListener(new View.OnClickListener() {
+        //adding onClickListener to button
+        sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent sndActivityIntent = new Intent(getApplicationContext(), SecondScreen.class);
-                startActivity(sndActivityIntent);
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.setType("text/plain");
+                sendIntent.putExtra(Intent.EXTRA_TEXT, ((EditText)findViewById(R.id.sndText)).getText().toString());
+
+                //this verification is needed, otherwise if there is no application capable of handling this intent the application will crash
+                if (sendIntent.resolveActivity(getPackageManager()) != null){
+                    startActivity(sendIntent);
+                }
             }
         });
 
